6eab7b01fb061748e3269a888fe436c0f377a45f
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-08 14:27:37 -0300

    Create Nokia.cs

---
+++ CelularPOO/Models/Nokia.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CelularPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando \"{nome}\" no Nokia via Nokia Store...");
+        }
+    }
+}
