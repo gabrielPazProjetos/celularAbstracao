11343d71772d74be50cdd922d6daad2022ac407e
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-11-08 14:28:11 -0300

    Create Iphone.cs

---
+++ CelularPOO/Models/Iphone.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CelularPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"Instalando \"{nome}\" no iPhone via App Store...");
+        }
+    }
+}
